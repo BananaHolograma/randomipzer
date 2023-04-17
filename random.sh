465e7ce4d8cf51b19f60b4c6f3d3e756eef01aae
0xp1n <s3r0s4pi3ns@protonmail.com>
2023-04-17 16:55:06 +0100

    Multiple options to be more flexible in the script execution

--- random.sh
+++ random.sh
@@ -20,7 +20,7 @@ function random_ipv6() {
 
 function randomize_ipv4_set() {
     declare -i times=$1
-    local delimiter=${2:-','}
+    local delimiter=${2:-'\n'}
     local ip_addresses=''
 
     for index in $(seq 0 $times); do 
@@ -28,12 +28,12 @@ function randomize_ipv4_set() {
         [[ $index -lt $times ]] && ip_addresses+="$delimiter"
     done 
 
-    echo -e "$ip_addresses"
+    echo -e "$ip_addresses" | sort -u
 }
 
 function randomize_ipv6_set() {
     declare -i times=$1
-    local delimiter=${2:-','}
+    local delimiter=${2:-'\n'}
     local ip_addresses=''
 
     for index in $(seq 0 $times); do 
@@ -41,5 +41,88 @@ function randomize_ipv6_set() {
         [[ $index -lt $times ]] && ip_addresses+="$delimiter"
     done 
 
-    echo -e "$ip_addresses"
+    echo -e "$ip_addresses" | sort -u
 }
+
+show_help() {
+    cat <<'EOF'
+USAGE:
+    randomipzer [OPTIONS]
+    Delimiter '\n' is set by default
+    ipv4 mode is selected by default
+
+EXAMPLES:
+    randomipzer --times 20 
+    randomipzer -t 100 -d '\n'
+    ramdomipzer --times 50 --delimiter ' ' --mode ipv6
+
+OPTIONS:
+    -m  --mode   <ipv4,ipv6,both>     Choose the ip category to randomly generate the values
+    -t, --times   <int>               Number of ip addresses to generate
+    -d  --delimiter <value>           Delimiter to separate the ip addresses in the output
+    -v  --version                     Display the actual version
+    -h  --help                        Print help information
+EOF
+}
+
+
+declare -gi TIMES
+declare -g  MODE='ipv4'
+
+to_lowercase() {
+    echo "$1" | tr '[:upper:]' '[:lower:]'
+}
+
+set_mode() {
+    declare -a available_modes=("ipv4" "ipv6" "both")
+    declare -i valid_mode=0
+    local selected_mode
+    selected_mode=$(to_lowercase "$1")
+
+    for mode in "${available_modes[@]}"; do
+        if [ "$mode" = "$selected_mode" ]; then
+            MODE=$mode
+            valid_mode=1
+            break
+        fi
+    done
+
+    if [ $valid_mode -eq 0 ]; then
+        echo -e "The selected mode $selected_mode is invalid, allowed values are: ${available_modes[*]}. The default mode $MODE will be used instead"
+    fi
+}
+
+
+for arg in "$@"; do
+  shift
+  case "$arg" in
+     '--mode')           set -- "$@" '-m'   ;;
+     '--times')          set -- "$@" '-t'   ;;
+     '--delimiter')      set -- "$@" '-d'   ;;
+    '--help')            set -- "$@" '-h'   ;;
+    *)                   set -- "$@" "$arg" ;;
+  esac
+done
+
+while getopts ":t:m:d:h:" arg; do
+    case $arg in
+        t) TIMES="$OPTARG";;
+        m) set_mode "$OPTARG";;
+        d) DELIMITER=${OPTARG:-'\n'};;
+        h | *)
+            show_help
+        ;;
+    esac
+
+done
+shift $(( OPTIND - 1))
+
+
+if [ "$MODE" = 'ipv4' ]; then
+    randomize_ipv4_set $TIMES "$DELIMITER"
+elif [ "$MODE" = 'ipv6' ]; then 
+    randomize_ipv6_set $TIMES "$DELIMITER"
+else 
+    randomize_ipv4_set $TIMES "$DELIMITER"
+    randomize_ipv6_set $TIMES "$DELIMITER"
+fi
